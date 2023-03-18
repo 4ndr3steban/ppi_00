bd93421b5018d2ea88c06b356f0ef08906cefaad
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2023-03-18 10:27:46 -0500

    avance app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask import render_template
-
+import os
 
 app = Flask(__name__)
 
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+@app.route('/css/<archivo>')
+def css_link(archivo):
+    return send_from_directory(os.path.join('templates/css'), archivo)
+
+@app.route('/images/<img>')
+def img_link(img):
+    return send_from_directory(os.path.join('templates/images'), img)
+
 
 @app.route('/catalogo')
 def catalogo():
