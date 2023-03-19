73a900df3f495fd59e020a0d4550eb3bad5df35c
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2023-03-19 16:46:39 -0500

    rutas de la barra de navegación

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+@app.route('/catalogo')
+def catalogo():
+    return render_template('2.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/contactanos')
+def contactanos():
+    return render_template('contactanos.html')
+
 @app.route('/css/<archivo>')
 def css_link(archivo):
     return send_from_directory(os.path.join('templates/css'), archivo)
@@ -16,16 +28,6 @@ def css_link(archivo):
 @app.route('/images/<img>')
 def img_link(img):
     return send_from_directory(os.path.join('templates/images'), img)
-
-
-@app.route('/catalogo')
-def catalogo():
-    return render_template('catalogo.html')
-
-@app.route('/nosotros')
-def nosotros():
-    return render_template('nosotros.html')
-
 
 
 
