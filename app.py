8fc4b198421a233c6a8276416f8b7659f2d75f71
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2023-03-30 19:18:38 -0500

    avance busqueda

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# --------------Librerias-------------------
 from flask import Flask, send_from_directory
-from flask import render_template
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 import os
+from webScrapping.MercadoLibre import main_ML 
+
 
 
 # instanciación de la aplicación
@@ -12,48 +13,53 @@ mysql = MySQL()
 
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = "Juan1234"
+app.config["MYSQL_DATABASE_PASSWORD"] = ""
 app.config["MYSQL_DATABASE_DB"] = "productos"
 
 mysql.init_app(app)
 
 # Ruta para la pagina inicial
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def inicio():
     return render_template('index.html') # Se retorna el html de la pagina de incio
 
+
+@app.route("/buscar-producto", methods = ["GET", "POST"])
+def buscar_producto():
+    if request.method == "POST":
+        busqueda = request.form['busqueda']
+      
+
+
 # Ruta para la pagian del catalogo
-@app.route('/catalogo')
+@app.route('/catalogo', methods = ["Get"])
 def catalogo():
 
-    conexion = mysql.connect()
-    cursor = conexion.cursor()
-    cursor.execute("""SELECT * FROM audifonossamsung""")
-    products = cursor.fetchall()
-    conexion.commit()
+    return render_template('catalogo.html') # Se retorna el html de la pagina de catalogo
 
-    return render_template('catalogo.html', products = products) # Se retorna el html de la pagina de catalogo
 
 # Ruta para la pagian de nosotros
-@app.route('/nosotros')
+@app.route('/nosotros', methods = ["Get"])
 def nosotros():
     return render_template('nosotros.html') # Se retorna el html de la pagina de nosotros
 
+
 # Ruta para la pagian de contactanos 
-@app.route('/contactanos')
+@app.route('/contactanos', methods = ["Get"])
 def contactanos():
     return render_template('contactanos.html') # Se retorna el html de la pagina de contactanos
 
+
 # Ruta para reconocer y usar los archivos css
-@app.route('/css/<archivo>')
+@app.route('/static/css/<archivo>', methods = ["Get"])
 def css_link(archivo):
-    return send_from_directory(os.path.join('templates/css'), archivo) # Se retorna la direccion a la carpeta de archivos css
+    return send_from_directory(os.path.join('templates/static/css'), archivo) # Se retorna la direccion a la carpeta de archivos css
+
 
 # Ruta para reconocer y usar las imagenes
-@app.route('/images/<img>')
+@app.route('/static/images/<img>', methods = ["Get"])
 def img_link(img):
-    return send_from_directory(os.path.join('templates/images'), img) # Se retorna la direccion a la carpeta de las imagenes
-
+    return send_from_directory(os.path.join('templates/static/images'), img) # Se retorna la direccion a la carpeta de las imagenes
 
 
 if __name__ == "__main__":
